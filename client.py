2233902dfc8c0c74c0692e48c5d2baa6661b20d5
Nakusu <leopold.lepage@gmail.com>
2021-05-12 00:25:24 +0200

    update of all

--- client.py
+++ client.py
@@ -2,40 +2,43 @@ import socket, sys, os
 from datetime import datetime
 import subprocess
 
-hote = "localhost"
-port = 4579
-end = True
-connect = False
-now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-print(now, " : Waiting server...")
-while connect == False:
-  try:
-    socket.connect((hote, port))
-    connect = True
-  except:
-    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    print(now, " : Waiting server...")
-    continue
-
-now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-print(now, " : Connect to the server !")
-
-socket.send("New connection".encode())
-while end != False:
-  response = socket.recv(255).decode()
-  if response == "close":
-    end = False
-    socket.close()
-    sys.exit(0)
+def main():
+  hote = "localhost"
+  port = 4579
+  end = True
+  connect = False
   now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-  print(now, " : " ,response)
 
-  try:
-    result = subprocess.check_output(['powershell.exe', response]).decode()
-    socket.send(result.encode())
-  except:
-    socket.send("error decode...".encode())
-    continue
+  conct = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  while connect == False:
+    try:
+      conct.connect((hote, port))
+      connect = True
+    except:
+      now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+      print(now, " : Waiting server...")
+      continue
+
+  now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+  print(now, " : Connect to the server !")
+
+  conct.send("New connection".encode())
+  while end != False:
+    response = conct.recv(255).decode()
+    if response == "close":
+      end = False
+      conct.close()
+      sys.exit(0)
+    now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    print(now, " : " ,response)
+
+    try:
+      result = subprocess.check_output(['powershell.exe', response]).decode()
+      socket.send(result.encode())
+    except:
+      socket.send("error decode...".encode())
+      continue
+
+if __name__ == "__main__":
+  main()
--- server.py
+++ server.py
@@ -1,29 +1,46 @@
 import socket, sys
+from IPy import IP
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(('0.0.0.0', 4579))
+def main(ip, port):
+  conct = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  conct.bind((ip, int(port)))
 
-print("--- STARTUP SERVER ---\n")
-while True:
-        socket.listen(5)
-        client, address = socket.accept()
-        print("- New connection")
+  print("--- STARTUP SERVER ---\nIp : ", ip, "\nPort : ", str(port), "\nstatus : Wait a connection")
+  while True:
+    conct.listen(5)
+    client, address = conct.accept()
+    print("- New connection")
 
-        response = client.recv(255)
-        if response != "":
-          content = ""
-          while content != "close":
-            content = input("Server : ")
-            client.send(content.encode())
-            response = client.recv(255).decode()
-            print("Client : ", response)
-          sys.exit(0)
-        elif response == "close":
-          client.close
-        elif response == "end":
-          stock.close
-        
+    response = client.recv(255)
+    if response != "":
+      content = ""
+      while content != "close":
+        content = input("Server : ")
+        client.send(content.encode())
+        response = client.recv(255).decode()
+        print("Client : ", response)
+        sys.exit(0)
+    elif response == "close" or response == "end":
+      client.close
+      conct.close
+      print("Close")
+          
 
-print("Close")
-client.close()
-stock.close()
+  client.close()
+
+def validIp(ip: str):
+  ips = ip.split('.')
+  if ip.replace(".", "").isnumeric() == False:
+    return False
+  for tmp in ips:
+    if int(tmp) > 255 or int(tmp) < 0:
+      return False
+  return True
+
+if __name__ == "__main__":
+  ip = input("Listen ip (0.0.0.0): ")
+  port = input("Listen port : ")
+  while port.isnumeric() == False or validIp(ip) == False:
+    ip = input("Listen ip (0.0.0.0): ")
+    port = input("Listen port : ")
+  main(ip, port)
